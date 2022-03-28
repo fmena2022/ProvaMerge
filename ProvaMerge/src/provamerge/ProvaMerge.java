b2a41b83686fa478f9d0229d26658d2a11b5d6b0
FMENA2022 <101583473+fmena2022@users.noreply.github.com>
2022-03-28 17:34:58 +0200

    Add files via upload

---
+++ ProvaMerge/src/provamerge/ProvaMerge.java
@@ -0,0 +1,12 @@
+
+package provamerge;
+
+public class ProvaMerge {
+    
+    public static void main(String[] args) {
+        System.out.println("Prova A");
+        System.out.println("Prova B");
+        System.out.println("Prova C");
+    }
+    
+}
