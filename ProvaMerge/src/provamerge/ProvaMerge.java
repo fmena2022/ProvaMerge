487428d82d5a4aecd80d1646b49db1c225799678
Francisco <Francisco@DESKTOP-IRNRCPH>
2022-03-31 18:42:22 +0200

    print C

--- ProvaMerge/src/provamerge/ProvaMerge.java
+++ ProvaMerge/src/provamerge/ProvaMerge.java
@@ -6,6 +6,7 @@ public class ProvaMerge {
     public static void main(String[] args) {
         System.out.println("Prova A");
         System.out.println("Prova B");
+        System.out.println("Prova C");
     }
     
 }
